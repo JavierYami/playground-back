7e071e1ffcbddca3793d2c23df721c6f000d8e39
JavierYami <javieryami1@outlook.com>
2023-08-09 11:27:05 -0600

    Conección con db

--- src/db.js
+++ src/db.js
@@ -1,16 +1,26 @@
 require('dotenv').config();
-
-import { Sequelize } from "sequelize";
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
     'playgrounddb',
-    'root@localhost',
+    'root',
     '',
      {
        host: 'localhost',
        dialect: 'mysql'
      }
    );
+
+// const authenticate = async () => {
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//       } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//       }
+// }
+
+// authenticate();
 
 module.exports = {
     sequelize,
